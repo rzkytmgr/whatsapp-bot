41bbd8c3d8c761cda8c4402aa2e690573c7710b0
rzkytmgr <rzkytmgr@gmail.com>
2021-03-31 21:22:01 +0700

    fix bug onGlobalParticipants issue #6

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const { create, decryptMedia } = require("@open-wa/wa-automate");
+const { create, decryptMedia } = require('@open-wa/wa-automate');
 
 const configObject = {
-  sessionId: "SAD_CLIENT",
+  sessionId: 'SAD_CLIENT',
   authTimeout: 0,
   autoRefresh: true,
   cacheEnabled: false,
-  chromiumArgs: ["--no-sandbox"],
+  chromiumArgs: ['--no-sandbox'],
   disableSpins: true,
   headless: true,
   qrRefreshS: 20,
@@ -13,25 +13,25 @@ const configObject = {
 };
 
 const ops = process.platform;
-if (ops === "win32" || ops === "win64") configObject["executeablePath"] = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe";
-else if (ops === "linux") configObject["executeablePath"] = "/usr/bin/google-chrome-stable";
-else if (ops === "darwin") configObject["executeablePath"] = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
+if (ops === 'win32' || ops === 'win64') configObject['executeablePath'] = 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe';
+else if (ops === 'linux') configObject['executeablePath'] = '/usr/bin/google-chrome-stable';
+else if (ops === 'darwin') configObject['executeablePath'] = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
 
 const startBot = async () => {
   try {
-    const Handler = require("./handler");
+    const Handler = require('./handler');
     const Client = await create(configObject);
 
     await Client.onStateChanged(async (state) => {
-      if (state === "TIMEOUT" || state === "CONFLICT" || state === "UNLAUNCHED") await Client.forceRefocus();
-      console.log("State Changed >", state);
+      if (state === 'TIMEOUT' || state === 'CONFLICT' || state === 'UNLAUNCHED') await Client.forceRefocus();
+      console.log('State Changed >', state);
     });
 
     await Client.onMessage((message) => {
       Handler.messageHandler(Client, message);
     });
 
-    await Client.onGlobalParicipantsChanged((event) => {
+    await Client.onGlobalParticipantsChanged((event) => {
       Handler.globalParticipantsChanged(Client, event);
     });
 
@@ -42,10 +42,10 @@ const startBot = async () => {
     await Client.onIncomingCall(async (call) => {
       const { peerJid } = call;
       await Client.contactBlock(peerJid);
-      await Client.sendText(peerJid, "_⚠️ Bot lagi sibuk, jangan Telpon! DM ke- *@rzkytmgrr* untuk Unblock!_");
+      await Client.sendText(peerJid, '_⚠️ Bot lagi sibuk, jangan Telpon! DM ke- *@rzkytmgrr* untuk Unblock!_');
     });
   } catch (error) {
-    console.log("Error When start bot " + error);
+    console.log('Error When start bot ' + error);
   }
 };
 
